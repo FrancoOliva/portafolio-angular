fe8f3d26e708ed1a280009fc77de36796afd8913
Franco Oliva <franco.oliva1991@gmail.com>
2021-05-05 09:25:15 -0300

    Sección 5 ok. Real time firebase petición get ok!

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,22 +10,29 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada: boolean = false;
 
-  constructor( private http: HttpClient ) { 
-    
-    //console.log("Servicio de infoPagina listo!");
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient ) {     
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  
+  private cargarInfo(){ // colocamos la petición Http que ya tenemos
 
     // leer archivo JSON -> necesitamos un modulo que nos permita hacer peticiones http
     // GET
     this.http.get('assets/data/data-pagina.json').subscribe( (resp:InfoPagina) => {
-
       this.cargada = true;
-      this.info = resp;
-     
-      console.log( resp );
+      this.info = resp;     
+    })    
+  }
 
+  private cargarEquipo(){
+    this.http.get('https://portafolio-angular-e320d-default-rtdb.firebaseio.com/equipo.json').subscribe( (respuesta:any) =>{
+
+    this.equipo = respuesta;
+    console.log(this.equipo);
     })
-    
-    
-
   }
 }
